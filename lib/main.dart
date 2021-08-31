1ca5e7ea0f7c5b889a66b731a6dc9926e05ed839
martru118 <50148537+martru118@users.noreply.github.com>
2021-08-31 16:22:01 -0400

    Change Main class to app name; initialize Firebase after initializing widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,41 @@
-import 'package:alarmdar/util/notifications.dart';
 import 'package:alarmdar/util/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'model/list_alarms.dart';
 
-void main() {
-  runApp(Main());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(Alarmdar());
 }
 
-class Main extends StatelessWidget {
-  final String title = "Alarmdar";
-
+class Alarmdar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Firebase.initializeApp(),
-      builder: (context, snapshot) {
-        //handle connection error
-        if (snapshot.hasError) {
-          print("Error initializing database");
-          return Text("Error initializing database");
-        }
+    final String title = this.runtimeType.toString();
 
-        //handle connection success
-        if (snapshot.connectionState == ConnectionState.done) {
-          NotificationService().init();
+    return MaterialApp(
+      title: title,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
 
-          return MaterialApp(
-            title: title,
-            debugShowCheckedModeBanner: false,
-            themeMode: ThemeMode.system,
+      //route settings
+      home: AlarmsList(title: title),
+      onGenerateRoute: (settings) => RouteGenerator.generateRoute(settings),
+      navigatorKey: RouteGenerator.navigatorKey,
 
-            //route settings
-            home: AlarmsList(title: title),
-            onGenerateRoute: (settings) => RouteGenerator.generateRoute(settings),
-            navigatorKey: RouteGenerator.navigatorKey,
-
-            //theme settings
-            theme: ThemeData(
-              brightness: Brightness.light,
-              primarySwatch: Colors.blue,
-              visualDensity: VisualDensity.adaptivePlatformDensity,
-            ),
-            darkTheme: ThemeData(
-              brightness: Brightness.dark,
-              primarySwatch: Colors.orange,
-              visualDensity: VisualDensity.adaptivePlatformDensity,
-            ),
-          );
-        } else {
-          return CircularProgressIndicator();
-        }
-      },
+      //theme settings
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
     );
   }
 }
